a3b05ac169fffd759aac637e2a42a530e381d1fa
Jon Jagger <jon@jaggersoft.com>
2014-01-14 16:30:25 +0000

    basic folder structure; start creates a primary-id (pid)

---
+++ client/views/nib.js
@@ -0,0 +1,26 @@
+
+newPid = function() {
+  return Random.hexString(4);  
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - 
+
+var page = {  
+  pid:function(arg) {
+    if (!arg) {
+      return $("#pid").val();
+    } else {
+      $("#pid").val(arg);
+    }
+  }
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - 
+
+Template.nib.events({"click #start":function () {
+  var nib = { pid:newPid(), size:"6" };
+  //Nibs.insert(nib);
+  page.pid(nib.pid);
+  #page.join().enable();
+}});
+
