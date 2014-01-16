22b707cd8590f196ebc4cd798e11a3eb73b94c84
Jon Jagger <jon@jaggersoft.com>
2014-01-16 16:06:54 +0000

    refactoring

--- nib.js
+++ nib.js
@@ -26,17 +26,19 @@ if (Meteor.isClient) {
   };
   
   Template.question.readonly = function() {
-    return valid(this.qid) ? "readonly" : "";     
+    return asked(this.qid) ? "readonly" : "";     
   };
   Template.question.text = function() {    
     var one = Questions.findOne({ qid:this.qid });
     return one ? one.text : question.instruction;
   };
   Template.question.disabled = function() {
-    return (valid(this.qid) || question.text() !== "") ? "disabled='disabled'" : ""; 
+    var yes = "disabled='disabled'";
+    var no = "";
+    return (asked(this.qid) || question.text() !== "") ? yes : no; 
   };
   Template.question.events({"keyup .question textarea":function() {
-    if (!valid(this.qid) && question.text() !== "") {
+    if (!asked(this.qid) && question.text() !== "") {
       question.button().enable();
     } else {
       question.button().disable();
@@ -53,28 +55,44 @@ if (Meteor.isClient) {
     return answered() ? "readonly" : "";
   };
   Template.answer.text = function() {
-    if (!valid(this.qid)) {
+    if (!asked(this.qid)) {
       return "";
-    } else if (answered()) {
-      return "";
-    } else {
+    } else if (!answered()) {
       return answer.instruction;
+    } else {
+      return "";
     }
   };
   Template.answer.disabled = function() {
-    return valid(this.qid) ? "" : "disabled='disabled'";
+    var disable = "disabled='disabled'";
+    if (!asked(this.qid)) {
+      return disable;
+    } else if (!answered()) {
+      return "";
+    } else {
+      return disable;      
+    }
   };  
   Template.answer.events({"click .answer input":function () {
     var text = answer.text();
     var one = { qid:this.qid, text:text };
-    if (text !== "") {
+    if (text !== answer.instruction && text !== "") {
       Answers.insert(one);
       answer.text("");
       answer.button().disable();
     }
     Session.set("answered", "true");
   }});
- 
+  
+  Template.countInfo.text = function() {
+    var n = Answers.find({qid:this.qid}).count();
+    if (asked(this.qid)) {      
+      return n + " " + plural("answer",n) + "...";
+    } else {
+      return "";
+    }
+  };
+
   Template.nib.allAnswers = function() {
     return Answers.find({ qid: this.qid });
   };
@@ -99,10 +117,14 @@ var question = {
   text:function() {
     return $(".question textarea").val();
   },
-  instruction:"type the question here",
+  instruction:"the question here",
   button:function() {
     return enabled($(".question input"));
   }
+};
+
+var asked = function(qid) {
+  return Questions.findOne({ qid:qid });
 };
 
 var answer = {
@@ -111,7 +133,7 @@ var answer = {
     if (arg !== undefined) { node.val(arg); }
     return node.val();
   },
-  instruction:"type your answer here",  
+  instruction:"your answer here",  
   button:function() {
     return enabled($(".answer input"));
   } 
@@ -121,7 +143,9 @@ var answered = function() {
   return Session.get("answered") === "true";
 };
 
-var valid = function(qid) {
-  return Questions.findOne({ qid:qid });
+var plural = function(word,n) {
+  return word + (n !== 1 ? "s" : "");  
 };
+
+
 
