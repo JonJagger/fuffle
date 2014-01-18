526187d2b910e962540501af8aeaba68e2158ec9
Jon Jagger <jon@jaggersoft.com>
2014-01-18 13:46:22 +0000

    hit enter same as pressing button

--- fuffle.js
+++ fuffle.js
@@ -14,26 +14,35 @@ Answers   = new Meteor.Collection('answers');
 
 if (Meteor.isClient) {
 
-  Template.ask.events({"keyup .ask input[type=text]":function() {
-    ask.button().enableIf(ask.text() !== "");
-  }});
   Template.ask.rendered = function() {
     $(".ask input[type=text]").select();
   };
+  Template.ask.events({"keyup .ask input[type=text]":function(event) {
+    var text = ask.text();
+    var isQuestion = text !== "";
+    ask.button().enableIf(isQuestion);
+    if (isQuestion && event.which === 13) {
+      var qid = saveQuestion(text);
+      Router.go('/answer/' + qid);      
+    }    
+  }});
   Template.ask.events({"click .ask input[type=button]":function () {
-    var qid = Random.hexString(6);
-    Questions.insert({ qid:qid, text:ask.text() });
+    var text = ask.text();
+    var qid = saveQuestion(text);
     Router.go('/answer/' + qid);
   }});
     
   Template.answer.rendered = function() {
     $(".answer input[type=text]").select(); // for focus
-  };
+  };  
+  Template.answer.events({"keyup .answer input[type=text]":function(event) {
+    if (event.which === 13) {
+      saveAnswer(this.qid, answer.text());
+      Router.go('/show/' + this.qid);      
+    }    
+  }});  
   Template.answer.events({"click .answer input[type=button]":function () {
-    var text = answer.text();
-    if (text !== "") {
-      Answers.insert({ qid:this.qid, text:text });
-    }
+    saveAnswer(this.qid, answer.text());
     Router.go('/show/' + this.qid);
   }});
     
@@ -75,9 +84,21 @@ var ask = {
   button:function() { return enabled($(".ask input[type=button]")); }
 };
 
+var saveQuestion = function(text) {
+  var qid = Random.hexString(6);
+  Questions.insert({ qid:qid, text:text });
+  return qid;
+}
+
 var answer = {
   text:function() { return $(".answer input[type=text]").val(); }
 };
+
+var saveAnswer = function(qid,text) {
+  if (text !== "") {
+    Answers.insert({ qid:qid, text:text });
+  }
+}
 
 var enabled = function(button) {
   button.enableIf = function(condition) {
